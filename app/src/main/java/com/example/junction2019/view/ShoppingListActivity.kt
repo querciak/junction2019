6f9df3aba206017db9a283c8c879a3ee7acf2a0b
andrea <andrea.cappello@studenti.unitn.it>
2019-11-16 19:34:30 +0200

    shopping list

--- app/src/main/java/com/example/junction2019/view/ShoppingListActivity.kt
+++ app/src/main/java/com/example/junction2019/view/ShoppingListActivity.kt
@@ -1,18 +1,23 @@
 package com.example.junction2019.view
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.junction2019.R
 import com.example.junction2019.presenter.ShoppinglistPresenter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ShoppingListActivity: AppCompatActivity() {
-
-    init {
-        var presenter: ShoppinglistPresenter? = ShoppinglistPresenter(_activity = this)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.shoppinglist_layout)
+
+        var presenter: ShoppinglistPresenter? = ShoppinglistPresenter(_activity = this)
+        presenter!!.test()
+
     }
+
+
 }

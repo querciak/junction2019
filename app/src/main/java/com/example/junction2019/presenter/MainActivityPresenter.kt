5fd42702eb10f72c53313dc47472c85ca20d87d3
andrea <andrea.cappello@studenti.unitn.it>
2019-11-16 12:59:33 +0200

    shitty code

---
+++ app/src/main/java/com/example/junction2019/presenter/MainActivityPresenter.kt
@@ -0,0 +1,11 @@
+package com.example.junction2019.presenter
+
+import android.view.View
+import com.example.junction2019.view.MainActivity
+
+class MainActivityPresenter( _view: View) {
+
+    private var view: View = _view
+    //private var model : ContractInterface.Model =
+
+}
---
+++ app/src/main/java/com/example/junction2019/view/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.junction2019.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.junction2019.R
+import com.example.junction2019.presenter.MainActivityPresenter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var presenter: MainActivityPresenter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //---------------------------------------------------------------------------------toolbar
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        actionBar!!.title = "Chose an index"
+    }
+
+
+
+
+}

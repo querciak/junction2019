83d93e584ef325328057f3710c8c8919f956c83d
andrea <andrea.cappello@studenti.unitn.it>
2019-11-16 23:46:30 +0200

    search

---
+++ app/src/main/java/com/example/junction2019/model/Products.kt
@@ -0,0 +1,14 @@
+package com.example.junction2019.model
+
+class Products(_name: String) {
+
+        // DUMMY RESPONSES
+        var name: String = _name
+
+        fun getProduct(): String{
+            return name
+
+
+
+    }
+}
